b0da8b68c35c6d9e240129172d131ed32a7d2f19
Berkan Izgi <biz3926@students.fhv.at>
2025-03-23 17:40:02 +0100

    unnötiger ordner

--- battleship/battleship/src/main/java/com/example/battleship/repository/ShipRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.battleship.repository;
-
-import com.example.battleship.model.Ship;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShipRepository extends JpaRepository<Ship, Long>{
-}
--- battleship/battleship/src/main/java/com/example/battleship/repository/ShotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.battleship.repository;
-
-import com.example.battleship.model.Shot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShotRepository extends JpaRepository<Shot, Long>{
-}
